c05a682f48e10a66201cc7b46dc7f39034c44226
ClaireMathiron <claira.m@live.fr>
2022-01-17 15:02:44 +0100

    initial commit

---
+++ src/views/Action.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="big_ctn">
+    <p>Hi and welcome, brave Knight !<br /></p>
+    <h3>Take action against this dragon</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Action",
+  props: {
+
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>404 NOT FOUND</h1>
+    <myfooter></myfooter>
+  </div>
+</template>
+
+<script>
+import myfooter from "../components/myfooter";
+
+export default {
+  name: "notfound",
+  components: {
+    myfooter
+  }
+};
+</script>
+<style scoped>
+div {
+  background-color: whitesmoke;
+}
+h1 {
+  color: red;
+}
+</style>
