c518d263c7872072e73ee4222e631fc159d19e6f
ClaireMathiron <claira.m@live.fr>
2022-01-17 15:02:44 +0100

    initial commit

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>404 NOT FOUND</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "NotFound",
+  components: {
+  }
+};
+</script>
+<style scoped>
+div {
+  background-color: whitesmoke;
+}
+h1 {
+  color: red;
+}
+</style>
