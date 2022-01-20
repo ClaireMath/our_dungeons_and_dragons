d00c2a885d93d3084d700cdfb3666242c5724780
ClaireMathiron <claira.m@live.fr>
2022-01-20 17:40:18 +0100

    suppression de png inutiles

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="bigCtn">
-    <!-- <input
-      @click="throwTheDice()"
-      type="button"
-      class="btn"
-      v-model="Jettez les dés"
-    /> -->
     <p>Vous partez avec {{ startingLifePoints }} points de vie.</p>
     <br />
     <div class="main">
-      <!-- <img v-bind:src="'../assets/dice/' + randomDice1 + '.png'"/> -->
-      <!-- <img :src="require('../assets/dice/' + randomDice1 + '.png')" id="img1"/>
- <img :src="require('../assets/dice/' + randomDice2 + '.png')" id="img2"/> -->
 
       <div class="div_du_Gif1">
-        <img v-if="gif1" src="../assets/dice/dice.gif" alt="dé gif1" id="img1" />
+        <img
+          v-if="gif1"
+          src="../assets/dice/dice.gif"
+          alt="dé gif1"
+          id="img1"
+        />
         <div v-if="dice1" class="whiteBox">
-          <img :src="require('../assets/dice/' + randomDice1 + '.png')" id="img2"/>
+          <img
+            :src="require('../assets/dice/' + randomDice1 + '.png')"
+            id="img2"
+          />
         </div>
       </div>
 
       <div class="div_du_Gif2">
-        <img v-if="gif2" src="../assets/dice/dice.gif" alt="dé gif2" id="img1" />
+        <img
+          v-if="gif2"
+          src="../assets/dice/dice.gif"
+          alt="dé gif2"
+          id="img1"
+        />
         <div v-if="dice2" class="whiteBox">
-          <img :src="require('../assets/dice/' + randomDice2 + '.png')" id="img2"/>
+          <img
+            :src="require('../assets/dice/' + randomDice2 + '.png')"
+            id="img2"
+          />
         </div>
       </div>
-     
     </div>
 
     <br />
@@ -34,8 +40,8 @@
     <button @click="throwTheDice(1)">Jettez un dé</button>
     <button @click="throwTheDice(2)">Jettez 2 dés</button>
 
-    <p v-if="randomDice1">{{ randomDice1 }}</p>
-    <p v-if="randomDice2">{{ randomDice2 }}</p>
+    <!-- <p v-if="randomDice1">{{ randomDice1 }}</p>
+    <p v-if="randomDice2">{{ randomDice2 }}</p> -->
 
     <p class="paragraphe" v-html="MyJson.book[Id].paragraph"></p>
 
@@ -103,26 +109,24 @@ export default {
     getFirstLifePoints() {
       this.gif1 = true;
       this.gif2 = true;
-      console.log("getFirstLifePoints : gif1 :" + this.gif1);
-      console.log("getFirstLifePoints :  dé :" + this.dice1);
       this.throwTheDice(2);
       this.startingLifePoints = (this.randomDice1 + this.randomDice2) * 4;
     },
 
     diceAnim(nbOfDice) {
-      console.log("cest diceanim ici !");
+      // console.log("cest diceanim ici !");
       if (nbOfDice == 1) {
         this.gif1 = false;
         this.dice1 = true;
-        console.log("diceAnim : gif1 :" + this.gif1);
-        console.log("diceAnim : dé :" + this.dice1);
+        // console.log("diceAnim : gif1 :" + this.gif1);
+        // console.log("diceAnim : dé :" + this.dice1);
       } else {
         this.gif1 = false;
         this.gif2 = false;
         this.dice1 = true;
         this.dice2 = true;
-        console.log("diceAnim else car 2 dés : gif1 :" + this.gif1);
-        console.log("diceAnim else car 2 dés : dé : " + this.dice1);
+        // console.log("diceAnim else car 2 dés : gif1 :" + this.gif1);
+        // console.log("diceAnim else car 2 dés : dé : " + this.dice1);
       }
     },
 
@@ -133,29 +137,26 @@ export default {
         this.dice1 = false;
         this.dice2 = false;
         this.gif1 = true;
-        console.log("tets");
-        console.log("if throwthedice : gif1 :" + this.gif1);
-        console.log("if throwthedice : dé :" + this.dice1);
+        // console.log("if throwthedice : gif1 :" + this.gif1);
+        // console.log("if throwthedice : dé :" + this.dice1);
         this.randomDice1 = Math.floor(6 * Math.random()) + 1;
         setTimeout(this.diceAnim, 2000, 1);
       } else {
         this.playSound();
         // this.gif1 = true;
-         this.dice1 = false;
+        this.dice1 = false;
         this.dice2 = false;
         this.gif1 = true;
         this.gif2 = true;
-        console.log("else throwthedice : gif1 :" + this.gif1);
-        console.log("else throwthedice : dé :" + this.dice1);
+        // console.log("else throwthedice : gif1 :" + this.gif1);
+        // console.log("else throwthedice : dé :" + this.dice1);
         // this.dice1 = true;
         this.randomDice1 = Math.floor(6 * Math.random()) + 1;
         this.randomDice2 = Math.floor(6 * Math.random()) + 1;
 
-      
         // attention dans le settimeout, le paramètre de notre fonction diceAnim
         // se place après le temps en milisecondes, aussi curieux que cela puisse paraitre
-         setTimeout(this.diceAnim, 1500, 2);
-        
+        setTimeout(this.diceAnim, 1500, 2);
       }
     },
 
@@ -166,7 +167,7 @@ export default {
 
     JoueurHitEnemy() {
       this.enemyLifePoints = 20;
-      console.log(this.enemyLifePoints);
+      // console.log(this.enemyLifePoints);
       //A aller chercher dans le json quand il sera mis en forme
 
       //le joueur attaque l'ennemi
@@ -175,12 +176,12 @@ export default {
       //calcul du resultat
 
       let total = this.randomDice1 + this.randomDice2;
-      console.log("Vous attaquez de:  " + total + "total des dés");
+      // console.log("Vous attaquez de:  " + total + "total des dés");
       if (total >= 6) {
-        console.log("le coup est reussi");
+        // console.log("le coup est reussi");
         let damage = total - 6 + this.weapon - this.enemyArmor;
         this.enemyLifePoints = this.enemyLifePoints - damage;
-        console.log("il reste à l'ennemi :" + this.enemyLifePoints);
+        // console.log("il reste à l'ennemi :" + this.enemyLifePoints);
       }
     },
 
@@ -188,22 +189,22 @@ export default {
       this.throwTheDice(2);
 
       let total = this.randomDice1 + this.randomDice2;
-      console.log("total des dés : " + total);
+      // console.log("total des dés : " + total);
       if (total >= 6) {
-        console.log("le coup est reussi car score égal ou supérieur à 6");
+        // console.log("le coup est reussi car score égal ou supérieur à 6");
         let damage = total - 6 + this.enemyWeapon - this.playerArmor;
-        console.log("enemyweapon : " + this.enemyWeapon);
-        console.log("playerarmor : " + this.playerArmor);
-        console.log(
-          "damage = (total des dés moins 6 + l'arme de l'ennemi - l'armure du joueur)" +
-            damage
-        );
+        // console.log("enemyweapon : " + this.enemyWeapon);
+        // console.log("playerarmor : " + this.playerArmor);
+        // console.log(
+        //   "damage = (total des dés moins 6 + l'arme de l'ennemi - l'armure du joueur)" +
+        //     damage
+        // );
         this.remainingLifePoints = this.startingLifePoints - damage;
         this.lifePoints = this.remainingLifePoints;
-        console.log(
-          "lifepoints restants du héro : " + this.remainingLifePoints
-        );
-        console.log("lifepoints : " + this.lifePoints);
+        // console.log(
+        //   "lifepoints restants du héro : " + this.remainingLifePoints
+        // );
+        // console.log("lifepoints : " + this.lifePoints);
       }
     },
     fight(whoStart) {
@@ -242,7 +243,7 @@ h3 {
 }
 
 .bigCtn {
-  background-color: pink;
+  background-image: url("../assets/scrollBack.jpeg");
   width: 100%;
   padding: 10%;
   display: flex;
@@ -262,6 +263,7 @@ h3 {
 }
 
 #img1 {
+  border-radius: 12px;
   width: 150px;
   height: 150px;
 }
@@ -275,9 +277,8 @@ h3 {
   width: 80px;
   height: 80px;
 }
-.paragraphe
-{
+.paragraphe {
   height: 100vh;
-    overflow: auto;
+  overflow: auto;
 }
 </style>
